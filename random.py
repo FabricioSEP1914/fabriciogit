af5420dfbde69b06ec721fbea0f1535ad9fd5e9a
FabricioSEP1914 <98464148+FabricioSEP1914@users.noreply.github.com>
2022-03-16 11:04:10 -0300

    Add files via upload

---
+++ random.py
@@ -0,0 +1,13 @@
+#random.random() - Gera um numero aleatorio entre 0 e 1
+#random.random()
+
+import random
+import math
+
+
+#valor = math.ceil(100 * random.random())
+#print (valor)
+
+valor = random.choice(["abacaxi","mamão","cenoura","pessego","ovo"])
+
+print (valor)
